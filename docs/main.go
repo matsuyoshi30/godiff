93a092582c8a7d4f1a5a1c89f99a48c60aff257b
matsuyoshi <sfbgwm30@gmail.com>
2020-01-24 02:31:16 +0900

    refine

--- docs/main.go
+++ docs/main.go
@@ -1,35 +1,50 @@
 package main
 
 import (
-	"fmt"
+	"bufio"
+	"strings"
 	"syscall/js"
 
 	"github.com/matsuyoshi30/godiff"
 )
 
 func showDiff(this js.Value, i []js.Value) interface{} {
-	// get str
-	str1 := getStr("input1")
-	str2 := getStr("input2")
+	document := js.Global().Get("document")
+	input1 := document.Call("getElementById", "input1")
+	input2 := document.Call("getElementById", "input2")
 
-	// check diff
-	d, err := godiff.NewDiff(str1, str2)
-	if err != nil {
-		fmt.Println(err)
-		return nil
+	str1 := input1.Get("value").String()
+	str2 := input2.Get("value").String()
+
+	scanner1 := bufio.NewScanner(strings.NewReader(str1))
+	scanner2 := bufio.NewScanner(strings.NewReader(str2))
+
+	for scanner1.Scan() && scanner2.Scan() {
+		d, _ := godiff.NewDiff(scanner1.Text(), scanner2.Text())
+		setDiff(d)
 	}
-	diffs := d.ShowDiff()
 
-	// set diff
-	js.Global().Get("document").Call("getElementById", "output1").Set("textContent", diffs[0])
-	js.Global().Get("document").Call("getElementById", "output2").Set("textContent", diffs[1])
-	js.Global().Get("document").Call("getElementById", "output3").Set("textContent", diffs[2])
+	// TODO: implement show diff when len(str1) is different from len(str2)
+	// currently only use for same length str1 and str2
 
 	return nil
 }
 
-func getStr(id string) string {
-	return js.Global().Get("document").Call("getElementById", id).Get("value").String()
+func setDiff(d *godiff.Diff) {
+	document := js.Global().Get("document")
+
+	diffs := d.ShowDiff()
+	td1 := document.Call("createElement", "td")
+	td1.Set("innerHTML", diffs[0])
+	tr1 := document.Call("createElement", "tr")
+	tr1.Call("appendChild", td1)
+	document.Call("getElementById", "output1").Call("appendChild", tr1)
+	td2 := document.Call("createElement", "td")
+	td2.Set("innerHTML", diffs[1])
+	tr2 := document.Call("createElement", "tr")
+	tr2.Call("appendChild", td2)
+	document.Call("getElementById", "output2").Call("appendChild", tr2)
+
 }
 
 func registerCallbacks() {
